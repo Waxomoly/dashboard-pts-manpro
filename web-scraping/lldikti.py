56d8a338650cc22932d69e3f8bc423b2b0cf0b80
Kelly <88363184+Waxomoly@users.noreply.github.com>
2025-09-17 23:38:18 +0700

    chore: update instruction

--- web-scraping/lldikti.py
+++ web-scraping/lldikti.py
@@ -1,4 +1,4 @@
 # link: https://lldikti7.kemdikbud.go.id/rekap-data-pts
 # hasil akhir adalah 2 dataframe dengan kolom: 
-# 1. email | institution_name | prodi_code | prodi | jenjang | akreditasi_prodi
-# 2. email | institution_name | institution_code | akreditasi_institusi | link
+# 1. primary_key (increasing integer) | email | institution_name | institution_code | akreditasi_institusi | link
+# 2. PK_on_dataframe_one | prodi_code | prodi_name | jenjang | akreditasi_prodi
