3de0e8604ec1fdd697fe2ab1030919e8bee0e802
Kelly <88363184+Waxomoly@users.noreply.github.com>
2025-09-17 23:10:28 +0700

    feat: change format of dataframe for lldikti

--- web-scraping/lldikti.py
+++ web-scraping/lldikti.py
@@ -1,4 +1,4 @@
 # link: https://lldikti7.kemdikbud.go.id/rekap-data-pts
 # hasil akhir adalah 2 dataframe dengan kolom: 
-# 1. email | institution_name | prodi | jenjang | akreditasi_prodi
-# 2. email | institution_name | akreditasi_institusi
+# 1. email | institution_name | prodi_code | prodi | jenjang | akreditasi_prodi
+# 2. email | institution_name | institution_code | akreditasi_institusi
