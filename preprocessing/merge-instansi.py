40e7bedc46d4d28cc6b8b4be6123ad5edec6d0cd
Kelly <88363184+Waxomoly@users.noreply.github.com>
2025-10-11 01:05:27 +0700

    feat: merge institutions

--- preprocessing/merge-instansi.py
+++ preprocessing/merge-instansi.py
@@ -30,3 +30,64 @@ print("--- Unique Columns Per DataFrame (Excluding Common Keys) ---")
 print(f"Rencanamu Unique Columns: {sorted(list(unique_rencanamu))}")
 print(f"Quipper Unique Columns:   {sorted(list(unique_quipper))}")
 print(f"PDDIKTI Unique Columns:   {sorted(list(unique_pddikti))}")
+
+
+
+
+# temporary drop, erase after andika's fix .........
+df_institution_pddikti.drop(columns=['regency'], inplace=True, errors='ignore')
+# ...................................................
+
+
+
+# MERGING DATAFRAMES -----------------------------------------------------------
+
+# add province column to quipper
+df_institution_quipper['province'] = '-'
+
+# add campus_accreditation column to quipper and pddikti
+df_institution_quipper['campus_accreditation'] = '-'
+df_institution_pddikti['campus_accreditation'] = '-'
+
+# combine all into one big dataframe
+df_merged = pd.concat([df_institution_quipper, df_institution_rencanamu, df_institution_pddikti], ignore_index=True, sort=False)
+print(df_merged.columns)
+
+
+# erase duplicate institution_name rows, keep one with biggest average_yearly_fee.
+# also fill in the '-' or -1 columns if possible
+print(f"\nTotal rows before cleaning duplicates: {len(df_merged)}")
+df_merged.sort_values(by='average_yearly_fee', ascending=False, inplace=True)
+duplicate_mask = df_merged.duplicated(subset=['institution_name'], keep=False)
+df_duplicates = df_merged[duplicate_mask]
+df_merged = df_merged.drop_duplicates(subset=['institution_name'], keep=False)
+
+# try to fill in the '-' or -1 columns if possible
+for idx, row in df_duplicates.iterrows():
+    institution_name = row['institution_name']
+    if institution_name in df_merged['institution_name'].values:
+        for col in df_merged.columns:
+            if col in ['institution_code', 'institution_name']:
+                continue
+            val_existing = df_merged.loc[df_merged['institution_name'] == institution_name, col].values[0]
+            val_new = row[col]
+            if pd.isna(val_existing) or val_existing in ['-', -1, '']:
+                if not (pd.isna(val_new) or val_new in ['-', -1, '']):
+                    df_merged.loc[df_merged['institution_name'] == institution_name, col] = val_new
+    else:
+        df_merged = pd.concat([df_merged, pd.DataFrame([row])], ignore_index=True, sort=False)
+
+
+print(f"Total rows after cleaning duplicates: {len(df_merged)}")
+
+
+# merge with institution banpt (accreditation & )
+# load banpt data
+df_banpt = pd.read_csv(BASE_PATH + "banpt.csv")
+
+
+
+# save to csv
+output_path = BASE_PATH + "merged_institutions.xlsx"
+df_merged.to_excel(output_path, index=False)
+print(f"\nMerging Selesai! File final '{output_path}' telah dibuat.")
